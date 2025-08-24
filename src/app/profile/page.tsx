0a6f24d338f7eb22d0a44c591ee104ff56cb0ea4
pavelrahman619 <pavelrahman.dev@gmail.com>
2025-08-24 09:52:14 +0600

    remove password reset tab
    not needed as profile is logged in using otp

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -92,12 +92,12 @@ const Profile: React.FC<profileProps> = () => {
                 >
                   Address
                 </Tabs.Trigger>
-                <Tabs.Trigger
+                {/* <Tabs.Trigger
                   className="bg-white px-5 h-[45px] flex-1 flex items-center justify-center text-[15px] leading-none text-mauve11 select-none first:rounded-tl-md last:rounded-tr-md hover:text-violet11 data-[state=active]:text-violet11 data-[state=active]:shadow-[inset_0_-1px_0_0,0_1px_0_0] data-[state=active]:shadow-current data-[state=active]:focus:relative data-[state=active]:focus:shadow-[0_0_0_2px] data-[state=active]:focus:shadow-black outline-none cursor-default"
                   value="tab2"
                 >
                   Password
-                </Tabs.Trigger>
+                </Tabs.Trigger> */}
                 <Tabs.Trigger
                   className="bg-white px-5 h-[45px] flex-1 flex items-center justify-center text-[15px] leading-none text-mauve11 select-none first:rounded-tl-md last:rounded-tr-md hover:text-violet11 data-[state=active]:text-violet11 data-[state=active]:shadow-[inset_0_-1px_0_0,0_1px_0_0] data-[state=active]:shadow-current data-[state=active]:focus:relative data-[state=active]:focus:shadow-[0_0_0_2px] data-[state=active]:focus:shadow-black outline-none cursor-default"
                   value="tab3"
