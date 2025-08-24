6cec127e6c8b64358f5f440ba5ab9d21673a0136
pavelrahman619 <pavelrahman.dev@gmail.com>
2025-08-24 08:57:46 +0600

    feat: persist cart items
    presist cart across refresh page
    use redux-persist

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,4 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 
 import addToCart from './features/cart/addToCart';
 import changeSearchBarVisibilityState from './features/searchBar/searchBarVisibility';
@@ -8,17 +19,36 @@ import setArea from './features/area/area';
 import setSearchResults from './features/searchResults/searchResults';
 import setMedicineSearchResults from './features/searchResults/medicineSearchResults';
 
-export const store = configureStore({
-  reducer: {
-    addToCart: addToCart,
-    searchBarVisibility: changeSearchBarVisibilityState,
-    setIsLoggedIn: setIsLoggedIn,
-    changeAreaDropDownOpenState: changeAreaDropDownOpenState,
-    area: setArea,
-    searchResults: setSearchResults,
-    medicineSearchResults: setMedicineSearchResults,
-  },
+const rootReducer = combineReducers({
+  addToCart: addToCart,
+  searchBarVisibility: changeSearchBarVisibilityState,
+  setIsLoggedIn: setIsLoggedIn,
+  changeAreaDropDownOpenState: changeAreaDropDownOpenState,
+  area: setArea,
+  searchResults: setSearchResults,
+  medicineSearchResults: setMedicineSearchResults,
 });
+
+const persistConfig = {
+  key: 'infinimed_root',
+  storage,
+  whitelist: ['addToCart'], // only persist cart slice
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        // Ignore redux-persist action types
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
